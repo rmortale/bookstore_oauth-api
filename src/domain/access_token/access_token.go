02e64d9ad2328b41af6701cc967692cab0dc3357
Antonio <dolce.antonio@gmail.com>
2022-07-06 21:52:22 +0200

    added cassandra dao

---
+++ src/domain/access_token/access_token.go
@@ -0,0 +1,48 @@
+package access_token
+
+import (
+	"github.com/rmortale/bookstore_oauth-api/src/utils/errors"
+	"strings"
+	"time"
+)
+
+const (
+	expirationTime = 24
+)
+
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	UserId      int64  `json:"user_id"`
+	ClientId    int64  `json:"client_id"`
+	Expires     int64  `json:"expires"`
+}
+
+func (at *AccessToken) Validate() *errors.RestErr {
+	at.AccessToken = strings.TrimSpace(at.AccessToken)
+	if at.AccessToken == "" {
+		return errors.NewBadRequestError("invalid access token")
+	}
+	if at.UserId <= 0 {
+		return errors.NewBadRequestError("invalid userid")
+	}
+	if at.ClientId <= 0 {
+		return errors.NewBadRequestError("invalid ClientId")
+	}
+	if at.Expires <= 0 {
+		return errors.NewBadRequestError("invalid Expires")
+	}
+	return nil
+}
+
+func GetNewAccessToken() AccessToken {
+	return AccessToken{
+		AccessToken: "",
+		UserId:      0,
+		ClientId:    0,
+		Expires:     time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
+}
+
+func (at AccessToken) IsExpired() bool {
+	return time.Unix(at.Expires, 0).Before(time.Now().UTC())
+}
